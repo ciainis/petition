c16c900b19998a420c6e091a6eba24292ad7cfa6
Marco Crippa <m.crippa84@gmail.com>
2019-01-18 10:34:21 +0100

    secrets fixed

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
 const spicedPg = require("spiced-pg");
-const { dbUser, dbPass } = require("./secrets.json");
+// const { dbUser, dbPass } = require("./secrets.json");
 // const db = spicedPg(`postgres:${dbUser}:${dbPass}@localhost:5432/funkyduck`);
 
 let db;
 if (process.env.DATABASE_URL) {
     db = spicedPg(process.env.DATABASE_URL);
 } else {
+    const { dbUser, dbPass } = require("./secrets.json");
     db = spicedPg(`postgres:${dbUser}:${dbPass}@localhost:5432/funkyduck`);
 }
 
